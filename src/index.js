fa79ca5811cdc2a159d16e98bbbecf02bb21d64a
jean <jean.ma.1986@gmail.com>
2017-01-18 16:42:54 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -6,7 +6,24 @@ let game=new Phaser.Game(800,600,Phaser.AUTO,'demo-1',{
 		game.load.image('ball',ball);
 	},
 	create:()=>{
-		game.add.sprite(0,0,'ball');
+		let ballSprite=game.add.sprite(0,0,'ball');
+		ballSprite.anchor.set(0.5);
+		ballSprite.position.x=50;
+		ballSprite.position.y=50;
+
+		// apply velocity
+		// game.physics.enable(ballSprite,Phaser.Physics.ARCADE);
+		// ballSprite.body.velocity.x=100;
+
+		// listener sprite input
+		// ballSprite.inputEnabled=true;
+		// let textSprite=game.add.text(50,0,'',{fill:'#ffffff'});
+		// ballSprite.events.onInputDown.add(()=>{
+		// 	textSprite.text=`i am clicked ${new Date()}`;
+		// })
+
+		// sprite follow input
+
 	}
 });
 
